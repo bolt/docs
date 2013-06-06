86ae6272c57cca3561f26f09cd438a21fcbedc49
Bob den Otter <bobdenotter@gmail.com>
2013-06-06 09:18:53 +0200

    Fancybox the screenshots.

--- view/js/docs.js
+++ view/js/docs.js
@@ -4,4 +4,9 @@ jQuery(function($) {
 	SyntaxHighlighter.defaults['auto-links'] = false;
 	SyntaxHighlighter.all();
 
+    // Initialize the Fancybox shizzle, if present.
+    if(jQuery().fancybox) {
+        $('.fancybox, div.imageholder a').fancybox({ });
+    }
+
 });
