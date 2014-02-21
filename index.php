11a3b3c8ebfd05ba19802d173a4f374f7468e9a5
Bob den Otter <bobdenotter@gmail.com>
2014-02-21 16:43:21 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require_once('lib/util.php');
 require_once('lib/smartypants.php');
 require_once './vendor/autoload.php';
 
-$version = "1.4.4";
+$version = "1.5.0";
 
 $request = basename($_SERVER['REQUEST_URI']);
 
