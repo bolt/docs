bf25c07828f923832b673a18136eef03fe4b30be
Bob den Otter <bobdenotter@gmail.com>
2016-02-02 15:54:52 +0100

    Make the recursive thing simpler.
    As pointed out by @rossriley

--- src/Command/BuildDocumentation.php
+++ src/Command/BuildDocumentation.php
@@ -35,9 +35,11 @@ class BuildDocumentation extends Command
     {
         $versionsArray = [];
 
+        $slugify = new \Cocur\Slugify\Slugify();
+
         foreach ($input->getArgument('branches') as $branch) {
             $this->getSubTree($branch, $output, './source_docs/');
-            $versionsArray[$branch] = $foldername;
+            $versionsArray[$branch] = $slugify->slugify($branch);
         }
 
         $dumper = new Dumper();
@@ -54,34 +56,25 @@ class BuildDocumentation extends Command
 
         $slugify = new \Cocur\Slugify\Slugify();
 
-        $tree = shell_exec('git ls-tree '. $branch . " " . $prefix);
+        $tree = shell_exec('git ls-tree -r '. $branch . " " . $prefix);
         $tree = array_filter(explode("\n", $tree));
         $filelist = array_map('str_getcsv', $tree, array_fill(0, count($tree), "\t"));
 
         foreach ($filelist as $source) {
-            $filemeta = explode(' ', $source[0]);
+            $file = $source[1];
 
-            if ($filemeta[1] == 'blob') {
-                $file = $source[1];
+            $content = shell_exec('git show ' . $branch . ":" . $file);
 
-                $content = shell_exec('git show ' . $branch . ":" . $file);
+            $folderName = $slugify->slugify($branch);
 
-                $folderName = $slugify->slugify($branch);
-
-                @mkdir(dirname($directory . $folderName . '/' . $file), 0755, true);
-                file_put_contents($directory . $folderName . '/' . $file, $content);
-            } elseif ($filemeta[1] == 'tree') {
-                $this->getSubTree($branch, $output, './' . $source[1] . '/');
-            }
+            @mkdir(dirname($directory . $folderName . '/' . $file), 0755, true);
+            file_put_contents($directory . $folderName . '/' . $file, $content);
         }
 
-        // Only get the menu, if we're in the ./source_docs/ top level.
-        if ($prefix == './source_docs/') {
-            $menu = shell_exec('git show ' . $branch . ":menu_docs.yml");
-            file_put_contents($directory . $folderName . '/menu_docs.yml', $menu);
+        $menu = shell_exec('git show ' . $branch . ":menu_docs.yml");
+        file_put_contents($directory . $folderName . '/menu_docs.yml', $menu);
 
-            $output->writeln("<info>Branch $branch written to $directory$folderName</info>");
-        }
+        $output->writeln("<info>Branch $branch written to $directory$folderName</info>");
     }
 
 }
