4576fb0dcb554694596d93e62ae9bcfaa7e03847
Bob den Otter <bob@twokings.nl>
2016-02-04 19:12:13 +0100

    Working on Frontend matter.

---
+++ view/gulpfile.js
@@ -0,0 +1,49 @@
+var gulp   = require('gulp');
+var $      = require('gulp-load-plugins')();
+
+// Define base paths for Sass and Javascript.
+var sassPaths = [
+    'source/scss/',
+    'bower_components/foundation-sites/scss',
+    // 'bower_components/motion-ui/src',
+    // 'bower_components/slicknav/scss',
+    // 'bower_components/highlightjs/styles'
+];
+
+var javascriptFiles = [
+    'bower_components/jquery/dist/jquery.js'
+];
+
+// Set up 'sass' task.
+gulp.task('sass', function() {
+  return gulp.src('scss/docs.scss')
+    .pipe($.sass({
+      includePaths: sassPaths,
+      outputStyle: 'compressed' // 'compressed' or 'nested'
+    })
+      .on('error', $.sass.logError))
+    .pipe($.autoprefixer({
+      browsers: ['last 2 versions', 'ie >= 9']
+    }))
+    .pipe(gulp.dest('styles'));
+});
+
+// Set up 'compress' task.
+gulp.task('compress', function() {
+  return gulp.src('source/javascript/*.js')
+    .pipe($.uglify())
+    .pipe(gulp.dest('javascript'));
+});
+
+
+gulp.task('copyjavascript', function() {
+   gulp.src(javascriptFiles)
+   .pipe(gulp.dest('source/javascript'));
+});
+
+
+// Set up 'default' task, with watches.
+gulp.task('default', ['sass', 'compress'], function() {
+  gulp.watch(['source/scss/**/*.scss'], ['sass']);
+  gulp.watch(['source/javascript/**/*.js'], ['compress']);
+});
