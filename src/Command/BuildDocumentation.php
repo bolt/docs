08912a193f3fffb709aa704bafbcb11b7ffa7ef9
Carson Full <carsonfull@gmail.com>
2016-04-16 15:12:53 -0500

    Change version name from "Bolt release-2-2" -> "2.2"

--- src/Command/BuildDocumentation.php
+++ src/Command/BuildDocumentation.php
@@ -2,7 +2,6 @@
 
 namespace Bolt\Docs\Command;
 
-use Cocur\Slugify\Slugify;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -16,9 +15,8 @@ use Symfony\Component\Yaml\Dumper;
  */
 class BuildDocumentation extends Command
 {
-
-    public $rootdir = '/version/';
-    public $versions = 'versions.yml';
+    private $versionsDir = '/version/';
+    private $versionsFile = 'versions.yml';
 
     protected function configure()
     {
@@ -28,54 +26,46 @@ class BuildDocumentation extends Command
     }
 
     /**
-     * @param InputInterface $input
-     * @param OutputInterface $output
-     * @return void
+     * {@inheritdoc}
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $versionsArray = [];
 
-        $slugify = new Slugify();
-
         foreach ($input->getArgument('branches') as $branch) {
-            $this->getSubTree($branch, $output, './source_docs/');
-            $versionsArray[$branch] = $slugify->slugify($branch);
+            $name = str_replace('release/', '', $branch);
+            $this->writeVersion($output, $branch, $name, './source_docs/');
+            $versionsArray[$branch] = $name;
         }
 
         $dumper = new Dumper();
 
         // Write the passed in versions to the yml file
-        $path = getcwd() . '/app/' . $this->versions;
+        $path = getcwd() . '/app/' . $this->versionsFile;
         file_put_contents($path, $dumper->dump($versionsArray));
         $output->writeln("<info>Versions saved to $path</info>");
     }
 
-    protected function getSubTree($branch, $output, $prefix = '')
+    protected function writeVersion(OutputInterface $output, $branch, $branchName, $prefix = '')
     {
-        $directory = getcwd() . $this->rootdir;
+        $directory = getcwd() . $this->versionsDir . $branchName;
 
-        $slugify = new Slugify();
-
-        $tree = shell_exec('git ls-tree -r ' . $branch . " " . $prefix);
+        $tree = shell_exec("git ls-tree -r $branch $prefix");
         $tree = array_filter(explode("\n", $tree));
         $filelist = array_map('str_getcsv', $tree, array_fill(0, count($tree), "\t"));
 
         foreach ($filelist as $source) {
             $file = $source[1];
 
-            $content = shell_exec('git show ' . $branch . ":" . $file);
+            $content = shell_exec("git show $branch:$file");
 
-            $folderName = $slugify->slugify($branch);
-
-            @mkdir(dirname($directory . $folderName . '/' . $file), 0755, true);
-            file_put_contents($directory . $folderName . '/' . $file, $content);
+            @mkdir(dirname($directory . '/' . $file), 0755, true);
+            file_put_contents($directory . '/' . $file, $content);
         }
 
         $menu = shell_exec('git show ' . $branch . ":menu_docs.yml");
-        file_put_contents($directory . $folderName . '/menu_docs.yml', $menu);
+        file_put_contents($directory . '/menu_docs.yml', $menu);
 
-        $output->writeln("<info>Branch $branch written to $directory$folderName</info>");
+        $output->writeln("<info>Branch $branch written to $directory</info>");
     }
-
 }
